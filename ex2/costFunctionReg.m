6abd416eac600e309f0bfc6190e0a7c6d249c7da
woong <anguish71@gmail.com>
2016-09-28 01:20:44 +0900

    Implement assignment2 for logistic regression

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,8 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+temp= sigmoid(X*theta);
+J = -1/m*(y'*log(temp)+(1-y)'*log(1-temp))+(lambda/(2*m))*sum(theta.^2);
 
-
+grad = (1/m).*((temp-y)'*X)'+(lambda/m).*theta;
+grad(1,1)-(lambda/m).*theta;
 
 
 
